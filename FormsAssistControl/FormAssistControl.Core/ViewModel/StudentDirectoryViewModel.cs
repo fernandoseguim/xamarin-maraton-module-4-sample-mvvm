344eb3eb2b4afefa04f5aa5ddc57865e5529c277
Fernando Seguim <fernando@seguim.it>
2017-03-14 15:54:02 -0300

    sample mvvm, module 4 - xamarin maraton

---
+++ FormsAssistControl/FormAssistControl.Core/ViewModel/StudentDirectoryViewModel.cs
@@ -0,0 +1,58 @@
+﻿using FormsAssistControl.Model.Entities;
+using FormsAssistControl.Model.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormsAssistControl.ViewModel
+{
+    public class StudentDirectoryViewModel : ObservableBaseObject
+    {
+
+        public ObservableCollection<Student> Students { get; set; }
+        public bool IsBusy
+        {
+
+            get
+            {
+                return IsBusy;
+            }
+
+            set
+            {
+                IsBusy = value; OnPropertyChanged();
+            }
+
+        }
+        public Command LoadDirectoryCommand { get; private set; }
+
+        public StudentDirectoryViewModel()
+        {
+            Students = new ObservableCollection<Student>();
+            IsBusy = false;
+            LoadDirectoryCommand = new Command((obj) => LoadDirectory());
+
+        }
+
+        async void LoadDirectory()
+        {
+            if (!IsBusy)
+            {
+                IsBusy = true;
+                await Task.Delay(3000);
+                var loadedDirectory = StudentDirectoryService.LoadStudentDirectory();
+
+                foreach (var student in loadedDirectory.Students)
+                {
+                    Students.Add(student);
+                }
+
+                IsBusy = false;
+            }
+        }
+    }
+}
---
+++ FormsAssistControl/FormsAssistControl/FormsAssistControl/Model/Services/StudentDirectoryService.cs
@@ -0,0 +1,45 @@
+﻿using FormsAssistControl.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsAssistControl.Model.Services
+{
+    public class StudentDirectoryService
+    {
+
+        public static StudentDirectory LoadStudentDirectory()
+        {
+
+            StudentDirectory studentDirectory = new StudentDirectory();
+            ObservableCollection<Student> students;
+
+            string[] names = { "José Luiz", "Miguel Angelo", "JOsé Francisco", "Jesus Antonio" };
+            string[] lastNames = { "Fernandes", "Garcia", "Matins", "Lopes", "Gonzales" };
+
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            students = new ObservableCollection<Student>();
+
+            for (int i = 0; i < 20; i++)
+            {
+
+                Student student = new Student();
+                student.Name = names[random.Next(0, 8)];
+                student.Lastname = $"{lastNames[random.Next(0, 5)]} {lastNames[random.Next(0, 5)]}";
+                student.Group = random.Next(465, 458).ToString();
+                student.StudentNumber = random.Next(12384748, 32384748).ToString();
+                student.Average = random.Next(100, 1000) / 10;
+                students.Add(student);
+                                      
+            }
+            studentDirectory.Students = students;
+            return studentDirectory;
+
+        }
+
+    }
+}
